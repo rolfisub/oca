b84856371694d70564e2c5707593db4aa40988d4
Rolf Bansbach <rolfisub@gmail.com>
2018-08-30 22:44:20 -0400

    some tests

---
+++ src/Animal.java
@@ -0,0 +1,16 @@
+public class Animal {
+    String name;
+
+    public static void main(String[] args) {
+        System.out.println("the main function");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+}
---
+++ src/Zoo.java
@@ -0,0 +1,7 @@
+public class Zoo {
+    public static void main(String[] args) {
+        for (int x = 0; x< args.length; x++) {
+            System.out.println(args[x]);
+        }
+    }
+}
