277d7e8eb477fe790916624f43453d1265c55ac0
Rolf Bansbach <rolfisub@gmail.com>
2018-09-05 23:40:41 -0400

    more

---
+++ src/CanCompile.java
@@ -0,0 +1,13 @@
+
+
+
+public class CanCompile {
+int count;
+public void CanCompile() {
+
+        count = 4;
+ }
+ public static void main(String[] args) {
+         CanCompile s = new CanCompile();
+         System.out.println(s.count);
+         } }
